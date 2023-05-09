8a3d6046adbdc6c7397b669000664fda584a75cc
shru <shruthishekar@gmail.com>
2023-05-09 10:56:27 -0600

    apiRoutes get,post and delete

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const router = require('express').Router();
+//const router = require('express').Router();
 
-//Get /notes returns notes.html file
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, '../../public/notes.html'));
-});
+//routing 
+module.exports = (app) => {
+    //Get /notes returns notes.html file
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(_dirname, '../Develop/public/notes.html'));
+    });
 
-//Get * returns index.html file
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-
-module.export = router;
+    //Get * returns index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+    });
+};
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 // Dependencies
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
-//sets express app
+//express configuration - tells node that we are creating an express server
 const app = express();
+//sets initial port
 const port = process.env.port || 3001;
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = resuire('./routes/htmlRoutes');
 
 //middleware for parsing JSON and urlencoded from incoming data 
 app.use(express.urlencoded({extended: true}));
-//parsing incoming json data 
-app.use(express.json);
+app.use(express.json());
 app.use(express.static('public'));
 
 //use routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+require('./Develop/routes/apiRoutes')(app);
+require('./Develop/routes/htmlRoutes')(app);
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
+
 //starts the server to begin listenig
 app.listen(port, () => {
-    console.log(`API server now on port ${port}!`);
+    console.log(`API server now on port: ${port}!`);
 });
